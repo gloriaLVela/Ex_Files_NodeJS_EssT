175d3e0a09781430e2fc99ded55081df05328286
gloriaLVela <gloria.laura.vela@gmail.com>
2020-02-18 10:54:52 -0600

    setInterval

--- ch03/03_07/start/timers.js
+++ ch03/03_07/start/timers.js
@@ -1,6 +1,14 @@
 const waitTime = 3000;
+const waitInterval = 500;
+let currentTime = 0;
+
+const incTime = () => {
+    currentTime += waitInterval;
+    console.log(`wating ${currentTime / 1000} seconds`);
+}
 console.log(`setting a ${waitTime / 1000} second delay`);
 
 const timerFinished = () => console.log("done");
 
+setInterval(incTime, waitInterval);
 setTimeout(timerFinished, waitTime);
